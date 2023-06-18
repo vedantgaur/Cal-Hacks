53bd50dc812c75e78675e7f4e33e51e9dd0e800f
Mihir Arya <msarya@icloud.com>
2023-06-18 01:29:07 -0700

    finalized sentiment analysis

--- src/Backend/util/sentiment_analysis.py
+++ src/Backend/util/sentiment_analysis.py
@@ -1,5 +1,5 @@
 from hume import HumeBatchClient
-from hume.models.config import LanguageConfig, FaceConfig
+from hume.models.config import LanguageConfig, FaceConfig, BurstConfig, ProsodyConfig
 from hume import HumeStreamClient
 import os
 from typing import Any, Dict, List, Iterator
@@ -8,30 +8,42 @@ from io import BytesIO
 from pathlib import Path
 
 
-def print_emotions(emotions: List[Dict[str, Any]]) -> None:
+def get_top_emotions(emotions: List[Dict[str, Any]]) -> None:
     emotion_map = {e["name"]: e["score"] for e in emotions}
-    for emotion in ["Joy", "Sadness", "Anger"]:
-        print(f"- {emotion}: {emotion_map[emotion]:4f}")
-
-def print_sentiment(sentiment: List[Dict[str, Any]]) -> None:
-    sentiment_map = {e["name"]: e["score"] for e in sentiment}
-    for rating in range(1, 10):
-        print(f"- Sentiment {rating}: {sentiment_map[str(rating)]:4f}")
+    # for emotion in emotion_map:
+    #     print(f"- {emotion}: {emotion_map[emotion]:4f}")
+    sorted_emotion_map = sorted(emotion_map, key=emotion_map.get, reverse=True)[:3]
+    return sorted_emotion_map
 
 def start_audio_sentiment_analysis_job(file_path):
     client = HumeBatchClient(os.getenv("HUME_API_KEY"))
-    # urls = ["https://storage.googleapis.com/hume-test-data/text/happy.txt"]
-    # webm_file = file_path
-    # mp3_file = os.system(f'ffmpeg -i "{webm_file}" -vn -ab 128k -ar 44100 -y "{webm_file[:-5]}.mp3";')
+    
+    burst_config = BurstConfig()
+    prosody_config = ProsodyConfig()
+
+    mp3_file = os.system(f'ffmpeg -i "{file_path}" -vn -ab 128k -ar 44100 -y "{file_path[:-5]}.mp3";')
     files = [file_path[:-5] + ".mp3"]
     
-    config = LanguageConfig(sentiment={}, granularity="sentence")
+    job = client.submit_job([], [burst_config, prosody_config], files=files)
+    print("Running...", job)
+
+    job.await_complete()
+    print("Job completed with status: ", job.get_status())
+    return job
+
+
+def start_text_sentiment_analysis_job(file_path):
+    client = HumeBatchClient(os.getenv("HUME_API_KEY"))
+    mp3_file = os.system(f'ffmpeg -i "{file_path}" -vn -ab 128k -ar 44100 -y "{file_path[:-5]}.mp3";')
+    files = [file_path[:-5] + ".mp3"]
+    
+    config = LanguageConfig(sentiment={}, granularity="utterance")
     job = client.submit_job([], [config], files=files)
     
     print("Running...", job)
 
     job.await_complete()
-    print("Job completed with status: ", job.get_predictions())
+    print("Job completed with status: ", job.get_status())
     return job
 
 def start_face_sentiment_analysis_job(file_path):
@@ -47,34 +59,45 @@ def start_face_sentiment_analysis_job(file_path):
     return job
 
 
-def get_job_predictions(job, model): #model = "language" for audio, "face" for video
+def get_job_predictions(job, models): #model = "language" for text, "face" for video, "prosody" + "burst" for audio
     full_predictions = job.get_predictions()
     transcription = ""
-    print(full_predictions)
-    for source in full_predictions:
-        # source_name = source["source"]["url"]
-        predictions = source["results"]["predictions"]
-        for prediction in predictions:
-            language_predictions = prediction["models"][model]["grouped_predictions"]
-            for language_prediction in language_predictions:
-                for chunk in language_prediction["predictions"]:
-                    if model=='language':
-                        transcription += f" {chunk['text']}"
-                        print(chunk["text"])
-                        print_emotions(chunk["emotions"])
-                        print("~ ~ ~")
-                        print_sentiment(chunk["sentiment"])
-                        print()
-    print(transcription)
+    # print(full_predictions)
+    for model in models:
+        for source in full_predictions:
+            # source_name = source["source"]["url"]
+            predictions = source["results"]["predictions"]
+            for prediction in predictions:
+                predictions = prediction["models"][model]["grouped_predictions"]
+                for prediction in predictions:
+                    for chunk in prediction["predictions"]:
+                        if model=='language':
+                            transcription += f" {chunk['text']}"
+                            # print(chunk["text"])
+                        top_emotions = get_top_emotions(chunk["emotions"])
+                        if model!='language':
+                            return top_emotions
+    return (transcription, top_emotions)
 
-def start_sentiment_analysis_job(file_path):
-    
-    job = start_audio_sentiment_analysis_job(file_path)
-    get_job_predictions(job, "language")
+def analyze_sentiment(file_path):
+    job = start_text_sentiment_analysis_job(file_path)
+    language_ouput = get_job_predictions(job, ["language"])
+    transcript = language_ouput[0]
+    language = language_ouput[1]
 
     job = start_face_sentiment_analysis_job(file_path)
-    get_job_predictions(job, "face")
+    face = get_job_predictions(job, ["face"])
 
+    job = start_audio_sentiment_analysis_job(file_path)
+    prosody = get_job_predictions(job, ["prosody"])
+    burst = get_job_predictions(job, ["burst"])
 
+    return {
+       "transcript" : transcript,
+       "language" : language,
+       "face": face,
+       "prosody": prosody,
+       "burst": burst
+    }
 
-start_sentiment_analysis_job("src/Backend/temp_bin/react-webcam-stream-capture.webm")
+print(analyze_sentiment("src/Backend/temp_bin/react-webcam-stream-capture.webm"))
