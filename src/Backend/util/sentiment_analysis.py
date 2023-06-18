701232ff71e2b1ca5629ef8808357d9030af504b
Mihir Arya <msarya@icloud.com>
2023-06-17 20:01:59 -0700

    sentiment analysis via local file + transcription

--- src/Backend/util/sentiment_analysis.py
+++ src/Backend/util/sentiment_analysis.py
@@ -1,5 +1,7 @@
 from hume import HumeBatchClient
 from hume.models.config import LanguageConfig
+from hume import HumeBatchClient, TranscriptionConfig
+from hume.models.config import ProsodyConfig
 import os
 from typing import Any, Dict, List
 
@@ -14,11 +16,12 @@ def print_sentiment(sentiment: List[Dict[str, Any]]) -> None:
     for rating in range(1, 10):
         print(f"- Sentiment {rating}: {sentiment_map[str(rating)]:4f}")
 
-def start_job():
+def start_sentiment_analysis_job():
     client = HumeBatchClient(os.getenv("HUME_API_KEY"))
-    urls = ["https://storage.googleapis.com/hume-test-data/text/happy.txt"]
+    # urls = ["https://storage.googleapis.com/hume-test-data/text/happy.txt"]
+    files = ["src/Backend/audio/hi.mp3"]
     config = LanguageConfig(sentiment={})
-    job = client.submit_job(urls, [config])
+    job = client.submit_job([], [config], files=files)
     
     print("Running...", job)
 
@@ -26,22 +29,24 @@ def start_job():
     print("Job completed with status: ", job.get_status())
     return job
 
-
 def get_job_predictions(job):
     full_predictions = job.get_predictions()
+    transcription = ""
     print(full_predictions)
     for source in full_predictions:
-        source_name = source["source"]["url"]
+        # source_name = source["source"]["url"]
         predictions = source["results"]["predictions"]
         for prediction in predictions:
             language_predictions = prediction["models"]["language"]["grouped_predictions"]
             for language_prediction in language_predictions:
                 for chunk in language_prediction["predictions"]:
                     print(chunk["text"])
+                    transcription += f" {chunk['text']}"
                     print_emotions(chunk["emotions"])
                     print("~ ~ ~")
                     print_sentiment(chunk["sentiment"])
                     print()
+    print(transcription)
 
-job = start_job()
+job = start_sentiment_analysis_job()
 get_job_predictions(job)
